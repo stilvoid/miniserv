3df5d6fe0b82a925a61d476e05784d365627710b
Steve Engledow <steve@offend.me.uk>
2012-06-15 12:17:56 +0100

    Change command line args format and accept a different index file

--- miniserv.js
+++ miniserv.js
@@ -2,70 +2,54 @@ var path = require("path");
 
 var nosef = require("nosef");
 
-var dir = "./";
-var ip = "127.0.0.1";
-var port = 8000;
+var args = {
+    path: "./",
+    host: "127.0.0.1",
+    port: 8000,
+    index: "index.html"
+};
 
-var host;
-
-if(process.argv.length == 3 && /^-?-h(elp)?$/.test(process.argv[2])) {
-    console.log("Usage: miniserv [PATH] [[HOST:]PORT]");
-    console.log("  path defaults to ./");
-    console.log("  host defaults to 127.0.0.1");
-    console.log("  port defaults to 8000");
+if(process.argv.length == 3 && /^--?h(elp)?$/.test(process.argv[2])) {
+    console.log("Usage: miniserv [PATH] [-h HOST] [-p PORT] [-i INDEX]");
+    console.log("  PATH defaults to " + args.path);
+    console.log("  HOST defaults to " + args.host);
+    console.log("  PORT defaults to " + args.port);
+    console.log("  INDEX defaults to " + args.index);
 
     process.exit();
 }
 
-if(process.argv.length == 3) {
-    if(isNaN(process.argv[2])) {
-        // See if it's a host:port
-        if(/^[\w\.]+(:\d+)?$/.test(process.argv[2])) {
-            host = process.argv[2];
-        } else {
-            dir = process.argv[2];
-        }
+for(var i=2; i<process.argv.length; i++) {
+    if(/^--?h(ost)?/.test(process.argv[i])) {
+        args.host = process.argv[++i];
+    } else if(/^--?p(ort)?/.test(process.argv[i])) {
+        args.port = process.argv[++i];
+    } else if(/^--?i(ndex)?/.test(process.argv[i])) {
+        args.index = process.argv[++i];
     } else {
-        // It's a number, probably a port
-        port = process.argv[2];
-    }
-} else if(process.argv.length == 4) {
-    dir = process.argv[2];
-    host = process.argv[3];
-}
-
-if(host) {
-    host = host.split(":");
-    if(host.length == 1) {
-        port = host[0];
-    } else if(host.length == 2) {
-        ip = host[0];
-        port = host[1];
-    } else {
-        console.log("Could not parse host:port \"" + host.join(":") + "\"");
-        process.exit();
+        args.path = process.argv[i];
     }
 }
 
-if(isNaN(port)) {
-    console.log("Invalid port number: " + port);
+if(isNaN(args.port)) {
+    console.log("Invalid port number: " + args.port);
     process.exit();
 }
 
 var config = {
-    port: port,
-    address: host,
+    port: args.port,
+    address: args.host,
     middleware: function(request, response) {
         console.log(request.url);
     },
     urls: [ // An array of arrays mapping URL patterns to handler functions
-        ["/", nosef.handlers.file(path.join(dir, "index.html"))],
-        ["/{{path}}", nosef.handlers.file(dir, "path")]
+        ["/", nosef.handlers.file(path.join(args.path, args.index))],
+        ["/{{path}}", nosef.handlers.file(args.path, "path")]
     ]
 };
 
 var server = nosef.server(config);
 
 server.on("start", function() {
-    console.log("Serving files from " + dir + " over " + ip + ":" + port);
+    console.log("Serving files from " + args.path + " over " + args.host + ":" + args.port);
 });
